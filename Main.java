1ac78648290e078fcaf57c035bb27876ed199b7d
Xandra888 <casey-ann_lumbera@dlsl.edu.ph>
2025-08-02 10:17:20 +0800

    1

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main{
+    public static void main (String[]args){
+        
+        String[] names = new String[10];
+        
+        names[0] = ("Tyrone");
+        names[1] = ("Casey");
+        names[2] = ("Xander");
+        names[3] = ("Michael");
+        names[4] = ("Edely");
+
+        for (int i = 0; i <names.length; i++) {
+            System.out.println(names[i]);
+        }
